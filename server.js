4084dc00a27511e1a61e2b74a3bbd95b005d92db
wierza <dawid@wierzycki.pl>
2023-11-03 15:07:02 +0100

    Add changes for replit

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const db = require('./db')
+const path = require('path');
 
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors());
 
 const testimonialsRoutes = require('./routes/testimonials.routes')
 const concertsRoutes = require('./routes/concerts.routes');
@@ -17,10 +18,16 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  });
+
 app.use((req, res) => {
     res.status(404).json({ message: 'Not found...' });
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
-});
+  });
