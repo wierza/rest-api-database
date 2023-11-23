25bd5dfe86ebdd23848f789ab8b20c8a2ca6a817
wierza <dawid@wierzycki.pl>
2023-11-23 15:37:03 +0100

    Add env

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const path = require('path');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 
